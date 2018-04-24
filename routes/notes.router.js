b72fc9655052f529d40b67ee31ab4cdb3c34ac2a
patersog <gianluca.paterson@gmail.com>
2018-04-24 17:32:53 -0400

    Update routes to use knex

--- routes/notes.router.js
+++ routes/notes.router.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const express = require('express');
 
@@ -6,108 +6,118 @@ const express = require('express');
 const router = express.Router();
 
 // TEMP: Simple In-Memory Database
-const data = require('../db/notes');
-const simDB = require('../db/simDB');
-const notes = simDB.initialize(data);
+// const data = require('../db/notes');
+// const simDB = require('../db/simDB');
+// const notes = simDB.initialize(data);
+
+const knex = require('../knex');
 
 // Get All (and search by query)
 router.get('/notes', (req, res, next) => {
-  const { searchTerm } = req.query;
+	const searchTerm = req.query.searchTerm;
 
-  notes.filter(searchTerm)
-    .then(list => {
-      res.json(list);
-    })
-    .catch(err => {
-      next(err);
-    });
+	knex.select('notes.id', 'title', 'content').from('notes')
+		.modify(function (queryBuilder) {
+			if (searchTerm) {
+				queryBuilder.where('title', 'like', `%${searchTerm}%`);
+			}
+		})
+		.orderBy('notes.id')
+		.then(results => {
+			res.json(results);
+		})
+		.catch(err => next(err));
 });
 
 // Get a single item
 router.get('/notes/:id', (req, res, next) => {
-  const id = req.params.id;
+	const id = req.params.id;
 
-  notes.find(id)
-    .then(item => {
-      if (item) {
-        res.json(item);
-      } else {
-        next();
-      }
-    })
-    .catch(err => {
-      next(err);
-    });
+	knex
+		.select()
+		.from('notes')
+		.where('id', `${id}`)
+		.then(results => {
+			res.json(results[0]);
+		})
+		.catch(err => {
+			next(err);
+		});
 });
 
 // Put update an item
 router.put('/notes/:id', (req, res, next) => {
-  const id = req.params.id;
+	const id = req.params.id;
 
-  /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateableFields = ['title', 'content'];
+	/***** Never trust users - validate input *****/
+	const updateObj = {};
+	const updateableFields = ['title', 'content'];
 
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
+	updateableFields.forEach(field => {
+		if (field in req.body) {
+			updateObj[field] = req.body[field];
+		}
+	});
 
-  /***** Never trust users - validate input *****/
-  if (!updateObj.title) {
-    const err = new Error('Missing `title` in request body');
-    err.status = 400;
-    return next(err);
-  }
+	/***** Never trust users - validate input *****/
+	if (!updateObj.title) {
+		const err = new Error('Missing `title` in request body');
+		err.status = 400;
+		return next(err);
+	}
 
-  notes.update(id, updateObj)
-    .then(item => {
-      if (item) {
-        res.json(item);
-      } else {
-        next();
-      }
-    })
-    .catch(err => {
-      next(err);
-    });
+	knex
+		.update(updateObj)
+		.from('notes')
+		.where('id',`${id}`)
+		.returning(['id','title','content','created'])
+		.then(results => {
+			res.json(results[0]);
+		})
+		.catch(err => {
+			next(err);
+		});
 });
 
 // Post (insert) an item
 router.post('/notes', (req, res, next) => {
-  const { title, content } = req.body;
+	const { title, content } = req.body;
 
-  const newItem = { title, content };
-  /***** Never trust users - validate input *****/
-  if (!newItem.title) {
-    const err = new Error('Missing `title` in request body');
-    err.status = 400;
-    return next(err);
-  }
+	const newItem = { title, content };
+	/***** Never trust users - validate input *****/
+	if (!newItem.title) {
+		const err = new Error('Missing `title` in request body');
+		err.status = 400;
+		return next(err);
+	}
 
-  notes.create(newItem)
-    .then(item => {
-      if (item) {
-        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
-      }
-    })
-    .catch(err => {
-      next(err);
-    });
+
+	knex
+		.insert([newItem])
+		.into('notes')
+		.returning(['id', 'title', 'content', 'created'])
+		.then(results => {
+			res.json(results[0]);
+		})
+		.catch(err => {
+			console.log(err);
+		});
 });
 
 // Delete an item
 router.delete('/notes/:id', (req, res, next) => {
-  const id = req.params.id;
+	const id = req.params.id;
 
-  notes.delete(id)
-    .then(() => {
-      res.sendStatus(204);
-    })
-    .catch(err => {
-      next(err);
-    });
+	knex
+		.del()
+		.from('notes')
+		.where('id',`${id}`)
+		.then(results => {
+			res.sendStatus(204);
+		})
+		.catch(err => {
+			next(err);
+		});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const express = require('express');
 const morgan = require('morgan');
@@ -12,7 +12,7 @@ const app = express();
 
 // Log all requests. Skip logging during
 app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
-  skip: () => process.env.NODE_ENV === 'test'
+	skip: () => process.env.NODE_ENV === 'test'
 }));
 
 // Create a static webserver
@@ -26,24 +26,24 @@ app.use('/api', notesRouter);
 
 // Catch-all 404
 app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // Catch-all Error handler
 // Add NODE_ENV check to prevent stacktrace leak
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: app.get('env') === 'development' ? err : {}
-  });
+	res.status(err.status || 500);
+	res.json({
+		message: err.message,
+		error: app.get('env') === 'development' ? err : {}
+	});
 });
 
 // Listen for incoming connections
 app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
+	console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
-  console.error(err);
+	console.error(err);
 });
