a1921ddc43cac00a2166ecfa341cb6b0e3a19667
patersog <gianluca.paterson@gmail.com>
2018-04-27 15:53:21 -0400

    Another push for travis

--- db/seedData.js
+++ db/seedData.js
@@ -3,5 +3,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 module.exports = function(file, user = 'dev') {
-	return exec(`psql -f ${file} ${process.env.TEST_DATABASE_URL}` );
+	//return exec(`psql -U ${user} -f ${file} -d ${knex.client.connectionSettings.database}`);	
+	//return exec(`psql -f ${file} ${process.env.TEST_DATABASE_URL}` );
+	return exec(`psql -f ${file} -d postgres://dev:a4Qxr3mns@localhost/noteful-test`);
 };
